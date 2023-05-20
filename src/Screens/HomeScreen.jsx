9122562e09d8a5a1aa6b46298ddc4d3d59a7405f
Ambika2510 <kumarambika2510@gmail.com>
2023-05-20 22:08:23 +0530

    filter

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -10,6 +10,8 @@ const HomeScreen = () => {
     const [duplicatehotels, setduplicatehotels] = useState([]);
     const [fromdate, setfromdate] = useState('undefined');
     const [todate, settodate] = useState('undefined');
+    const [search, setsearch] = useState('');
+    const [type, settype] = useState('all');
     useEffect(() => {
       axios.get("http://localhost:3700/api/getallrooms")
       .then((res)=>{
@@ -18,6 +20,20 @@ const HomeScreen = () => {
         setduplicatehotels(res.data)
       });
     },[])
+    const filterBysearch=()=>{
+      const temphotels=duplicatehotels.filter((hotel)=>hotel.name.toLowerCase().includes(search.toLowerCase()));
+      sethotels(temphotels);
+    }
+    const filterbytype=(e)=>{
+    settype(e);
+      if(e==='all')
+     {sethotels(duplicatehotels); }
+      else{
+        const temphotels=duplicatehotels.filter((hotel)=>hotel.type.toLowerCase()===(e.toLowerCase()));
+      sethotels(temphotels);
+        
+      }
+    }
    const filterbydate=(dates)=>{
       
     const s=moment(dates[0].$d).format('DD-MM-YYYY');
@@ -41,7 +57,18 @@ const HomeScreen = () => {
     <div>
       <Navbar/>
       <div className='  flex justify-center m-3 p-2'>
-<div className='border-2 border-solid border-black   '>  <RangePicker format='DD-MM-YYYY' className='p-2'  onChange={filterbydate} /></div>
+<div className=' flex justify-center border-2 border-solid border-black   '>  <RangePicker format='DD-MM-YYYY' className='p-2'  onChange={filterbydate} /></div>
+<div>
+            <input type="text" id="first_name" className="bg-gray-50 mx-4 border-2 border-solid border-black  text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search rooms" value={search} onChange={(e)=>{setsearch(e.target.value)}} onKeyUp={filterBysearch} required/>
+        </div>
+        <div className='mx-4'>
+          <select className='"bg-gray-50 mx-4  border-2 border-solid border-black  text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' value={type} onChange={(e)=>{filterbytype(e.target.value)}}>
+            <option value="all">All</option>
+            
+            <option value="delux">Delux</option>
+            <option value="non-delux">Non-Delux</option>
+          </select>
+        </div>
     </div>
 <div className="">
   {hotels.length>0?<div className='flex justify-center'><div className='m-3 '>{hotels.map((hotel)=> (<Front key={hotel._id} hotel={hotel} fromdate={fromdate} todate={todate}/>))}</div></div>:<h1 className="m-6 text-center text-lg font-semibold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">!!Sorry No More Hotels Available...</h1>}
