f00b961c8452ae5055ecfb3e8869d033013baa33
Ambika2510 <kumarambika2510@gmail.com>
2023-05-23 11:21:19 +0530

    naavbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -38,7 +38,7 @@ return(
           <Link to={"/profile/"+uid}className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{user.name}</Link>
         </li>
         <li>
-          <Link to={"/booking/"+uid} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Booking</Link>
+          <Link to={"/booking/"+uid} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">My Booking</Link>
         </li>
         <li>
         <button onClick={handleclick} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Logout</button>
