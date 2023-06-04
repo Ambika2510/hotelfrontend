934263689d582e31df092e8599370d1d18aae69b
Ambika2510 <kumarambika2510@gmail.com>
2023-06-04 11:46:34 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
        
           
          <p className="text-center   text-slate-100 text-xl font-medium">For any queries related contact : kumarambika2510@gmail.com </p>
-             
+         <p className="mt-2 text-center   text-slate-100 text-xl font-medium">Owner : Ambika Singh Rajput </p>
             
        
         <hr className="my-6 border-gray-200 lg:mx-auto dark:border-gray-700 lg:my-8" />
-        <div className="flex justify-center text-xl font-medium text-slate-100">© 2023 <a href="/home" className="hover:underline">Hotel Booking</a>. Owner : AMBIKA KUMAR.</div>
+        <div className="flex justify-center text-xl font-medium text-slate-100">© 2023 <a href="/home" className="hover:underline">Hotel Booking</a>.ALL Right Reserved</div>
     </div>
 </footer>
 
