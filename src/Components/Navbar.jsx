82b946e8a7e088d5c8a5017f14892c9087a905a2
Ambika kumar <95169962+Ambika2510@users.noreply.github.com>
2024-04-09 00:51:23 +0530

    website name update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,7 +31,7 @@ return(
   <div className="max-w-screen-full flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/home" className="flex items-center ">
         <img src={user.profileimage} className="w-12 h-12 rounded-full border-2 border-black mr-3 " alt="Flowbite Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Hotel booking</span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Hotel Recommendation</span>
     </a>
     <button onClick={()=>{setd(1-d)}}  type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
